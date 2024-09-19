3ef3c59a35f5dbbd94d5660dc7c78f7c35df2724
Wifth <alexander.obando.os2206@gmail.com>
2024-09-19 11:29:23 -0500

    Ejercicio 03

---
+++ Ejercicio03/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num, i = 0, p = 0, n = 0;
+
+do
+{
+    Console.Write("Ingresar un número: ");
+    num = int.Parse(Console.ReadLine());
+
+    if (num % 2 == 0 && num != 0)
+        p++;
+    else if (num % 2 != 0 && num != 0)
+        i++;   
+} while (num > 0);
+
+Console.WriteLine("\n\nCantidad de pares: " + p);
+Console.WriteLine("Cantidad de impares: " + i);
