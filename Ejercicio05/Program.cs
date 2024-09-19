05007638525233c30b9e30e3011232fd4d3c7875
Wifth <alexander.obando.os2206@gmail.com>
2024-09-19 11:58:14 -0500

    Ejercicio 05

---
+++ Ejercicio05/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+
+Random r = new Random();
+int num = r.Next(1, 21), n, intentos = 0;
+
+Console.WriteLine(num);
+Console.WriteLine("*******************************\n");
+Console.WriteLine("\tBienvenido.");
+Console.WriteLine("\tVamos a jugar!");
+Console.WriteLine("\n********************************");
+Console.WriteLine("Intente adivinar el número sacado al azar\n\n");
+
+do
+{
+    Console.Write("Ingrese su número: ");
+    n = int.Parse(Console.ReadLine());
+    if (num == n)
+    {
+        Console.WriteLine("");
+        Console.WriteLine("\n********************************");
+        Console.WriteLine("\tNúmero adivinado!");
+        Console.WriteLine("\tFelicidades!");
+        Console.WriteLine("\n********************************");
+        break;
+    }
+    else if (n != num && n > num)
+    {
+        Console.WriteLine("Tu número es mayor que el sacado.\n");
+        intentos++;
+    }
+    else if (n != num && n < num)
+    {
+        Console.WriteLine("Tu número es menor que el sacado.\n");
+        intentos++;
+    }
+
+
+} while (intentos < 3);
+
+Console.WriteLine("Juego terminado.");
