62604f77ed4ddf123b11957338c998168a21488e
Wifth <alexander.obando.os2206@gmail.com>
2024-09-19 11:21:03 -0500

    Ejercicio 02

---
+++ Ejercicio02/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics.CodeAnalysis;
+
+int num, suma = 0;
+
+do
+{
+    Console.WriteLine("Ingrese un número: ");
+    num = int.Parse(Console.ReadLine());
+
+    if (num > 0)
+        suma += num;
+
+} while (num != 0);
+
+Console.WriteLine("Suma de positivos: " + suma);
